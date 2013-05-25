a21ee2c6226614183c37772a1c4303c496d19d99
Kai Böse <kai.boese@web.de>
2013-05-25 22:34:32 +0200

    Added functionally to osu skin module
    Added some functions, such as load or compuile skin to the
    Skins::Osu-Module

--- Lib/System/Skinnables.rb
+++ Lib/System/Skinnables.rb
@@ -29,7 +29,7 @@ module Skins
 		
 		def self.synchronice
 			@syncs.each { |element|
-				Skins::Osu.add_default(@keys[element])
+				Skins::Osu.add_default(@keys[element][0])
 			}
 		end
 		
@@ -37,14 +37,43 @@ module Skins
 	
 	module Osu
 	
-		@keys = {}
+		@current_skin_files = []
+		@skin_probs = {}
 		
-		def self.get_file(id)
-			# laden der osu datei
+		@skinnable_dir = "#{$user_name}/OSC_Skin/"
+		@skin_files_dir = "Graphics/SkinFiles/"
+		
+		# the file named string from file.
+		def self.get_file(f_string)
+			f_path = @skinnable_dir + f_name
+			
+			overwritten = FileTest.exist?(f_path + ".png") || FileTest.exist?(f_path + ".jpg")
+			return overwritten ? f_path : (@skin_files_dir + f_string)
 		end
 		
-		def self.add_default(id)
-			# hinzufügen von dateien, die im skin ordner abgelegt sind und damit default
+		# add a filename as default to the skin which youre creting now
+		def self.add_default(f_name)
+			f_path = @skinnable_dir + f_name
+			
+			exist = FileTest.exist?(f_path + ".png") || FileTest.exist?(f_path + ".jpg")
+			@current_skin_files << f_path if exist
+		end
+		
+		def add_file(f_name)
+			@current_skin_files << f_path if FileTest.exist? f_name
+		end
+		
+		def self.set_properties(hash)
+			return unless hash.is_a? Hash
+			@skin_probs = hash
+		end
+		
+		def reset_skin
+			@current_skin_files.clear
+		end
+		
+		def self.compile
+			print "sort, then copy skin files into a skin folder"
 		end
 		
 	end
