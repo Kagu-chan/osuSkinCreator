29dfe2f20f85ce75a6d95f6c29ead3f316ccdb77
Kai Böse <kai.boese@web.de>
2013-06-09 23:36:51 +0200

    Skinnables
    fixed some stuff.

--- Lib/System/Skinnables.rb
+++ Lib/System/Skinnables.rb
@@ -10,21 +10,26 @@ module System
 			@keys = {:d_bg => ["D_Bg", "Pictures"],
 							 :mouse => ["cursor", "SkinFiles"],
 							 :w_skin => ["w_skin", "Windowskins"],
-							 :menu_back => ["menu-back", "SkinFiles"]
+							 :notes_skin => ["notes_skin", "Windowskins"],
+							 :menu_back => ["menu-back", "SkinFiles"],
+							 :menu_enter => ["play-skip", "SkinFiles"]
 							}
 			
 			# files which be related to osu skins
 			@syncs = [:mouse,
-								:menu_back]
+								:menu_back,
+								:menu_enter]
 			
 			def self.get_file(id)
 				return "" unless @keys.has_key?(id)
+				
+				return System::Skins::Osu.get_file(@keys[id][0]) if @syncs.include? id
 				
 				f_name = $user_name + "/OSC_Skin/" + @keys[id][0]
 				exist = FileTest.exist?(f_name + ".png") || FileTest.exist?(f_name + ".jpg")
 				
 				std_name = "Graphics/#{@keys[id][1]}/#{@keys[id][0]}"
-				std_name += FileTest.exist?(std_name + ".png") ? ".png" : ".jpn"
+				std_name += FileTest.exist?(std_name + ".png") ? ".png" : ".jpg"
 				
 				return exist ? f_name : std_name
 			end
@@ -47,7 +52,8 @@ module System
 			
 			# the file named string from file.
 			def self.get_file(f_string)
-				f_path = @skinnable_dir + f_string
+				skinnable_dir = "#{$user_name}/OSC_Skin/"
+				f_path = skinnable_dir + f_string
 				
 				overwritten = FileTest.exist?(f_path + ".png") || FileTest.exist?(f_path + ".jpg")
 				
