2364e71561de77b617683087e4427bc90cdd870c
Kai Böse <kai.boese@web.de>
2013-06-09 23:38:43 +0200

    New Scene Skins
    a new and clean scene for skins creating. Created new module (namespace)
    "SkinCreating"

--- Lib/Scenes/Scene_Skins.rb
+++ Lib/Scenes/Scene_Skins.rb
@@ -3,23 +3,66 @@ module Scenes
 	class Skins
 		
 		def main
-			@context_window = Context_Skins.new(Window_Skins.new)
-			@context_window.setup
+		
+			@title_text = " "
+			@current_context = nil
+			@window = Window_Context.new
+			@next_context = SkinCreate::Context::StartUp.new
+			
+			@back = Sprite.new; @back.visible = false
+			@back.bitmap = Bitmap.new System::Skins::OSC.get_file(:menu_back)
+			
+			@back.x = 5
+			@back.y = 480 - @back.bitmap.height - 5
+			
+			@enter = Sprite.new; @enter.visible = false
+			@enter.bitmap = Bitmap.new System::Skins::OSC.get_file(:menu_enter)
+			
+			@enter.x = 640 - @enter.bitmap.width - 5
+			@enter.y = 480 - @enter.bitmap.height - 5
 			
 			Graphics.transition
 			loop do
 				break unless $scene == self
-				Input._update
 				Graphics.update
+				Input._update
 				update
 			end
 			Graphics.freeze
 			
-			@context_window.unload
+			@enter.dispose
+			@back.dispose
+			@current_context.unload
+			@window.dispose
+			@text.dispose if @text
+		end
+		
+		def refresh
+			@current_context.unload unload unless @current_context.nil?
+			@current_context = @next_context
+			@current_context.setup
+			
+			unless @title
+				@title = Sprite.new
+				@title.y = 52
+			end
+			@title.bitmap.clear unless @title.bitmap.nil?
+			
+			@title.bitmap = Bitmap.new(640, 26)
+			@title.bitmap.draw_text(@title.bitmap.rect, @current_context.text, 1)
+			
+			@enter.visible = @current_context.has_forward
+			@back.visible = @current_context.has_backward
 		end
 		
 		def update
-			@context_window.update
+			refresh unless @current_context == @next_context
+			
+			ret = @current_context.update
+			@next_context = ret unless ret.nil?
+			
+			@current_context.go_forward if @enter.visible && @enter.mouse_over? && Input.mouse?
+			@current_context.go_backward if @back.visible && @back.mouse_over? && Input.mouse?
 		end
 		
 	end
